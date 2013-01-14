6ce2943f732f37fb0ba2e2f5f08831a7c73763f6
Marcel de Graaf <mail@marceldegraaf.net>
2013-01-14 11:52:51 +0100

    Fix merchant spec

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,7 +4,7 @@ describe Sisow::Merchant do
 
   it "should retrieve payment methods" do
     VCR.use_cassette('merchant') do
-      Sisow::Merchant.payment_methods.should == %w(ideal sofort)
+      Sisow::Merchant.payment_methods.should == %w(ideal)
     end
   end
 
